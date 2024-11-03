081a94e2963ce3d7fa24992a740ca9738c1fdebd
IlanFrasca <ilanfrasca@hotmail.co.uk>
2024-11-03 13:54:52 +0000

    without tut commit
    .

--- Assets/Scripts/DialogManager.cs
+++ Assets/Scripts/DialogManager.cs
@@ -2,19 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
 public class DialogManager : MonoBehaviour
 {
     public static DialogManager Instance;
 
     public GameObject dialogBox;     // Reference to the UI dialog box
     public Text dialogText;          // Reference to the Text component in the dialog box
-    public float typingSpeed = 0.05f; // Speed of typing effect in seconds
 
     private string[] sentences;
     private int currentSentenceIndex = 0;
     private bool isDialogActive = false;
-    private bool isTyping = false;   // Track if text is still typing
 
     private void Awake()
     {
@@ -31,9 +28,9 @@ public class DialogManager : MonoBehaviour
 
     private void Update()
     {
-        // Use Space key to continue dialog
-        if (isDialogActive && !isTyping && Input.GetKeyDown(KeyCode.Space))
+        if (isDialogActive && Input.GetKeyDown(KeyCode.Space))
         {
+            Debug.Log("Space key pressed, showing next sentence...");
             DisplayNextSentence();
         }
     }
@@ -52,9 +49,8 @@ public class DialogManager : MonoBehaviour
     {
         if (currentSentenceIndex < sentences.Length)
         {
-            // Start typing the current sentence
-            StopAllCoroutines();  // Stop any ongoing typing coroutine before starting a new one
-            StartCoroutine(TypeSentence(sentences[currentSentenceIndex]));
+            dialogText.text = sentences[currentSentenceIndex];
+            Debug.Log("Displaying sentence: " + sentences[currentSentenceIndex]);
             currentSentenceIndex++;
         }
         else
@@ -62,21 +58,6 @@ public class DialogManager : MonoBehaviour
             Debug.Log("End of dialog reached.");
             EndDialog();
         }
-    }
-
-    private IEnumerator TypeSentence(string sentence)
-    {
-        dialogText.text = ""; // Clear existing text
-        isTyping = true;
-
-        // Type each character one by one
-        foreach (char letter in sentence.ToCharArray())
-        {
-            dialogText.text += letter;
-            yield return new WaitForSeconds(typingSpeed);
-        }
-
-        isTyping = false; // Typing complete, ready to move to the next sentence
     }
 
     public void EndDialog()
